7962581104c43b7d5ebd5ad6cd34b238175c3db6
34011-65-03 <34011-65-03@STA6301484.exchange.ad.afpanet>
2018-11-09 09:37:47 +0100

    HelloWorld

---
+++ src/pckg/Exercice1.java
@@ -0,0 +1,12 @@
+package pckg;
+
+public class Exercice1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Start of the program
+		System.out.print("Bonjour cher utilisateur, ceci est votre premier programme!");
+	}
+
+}
