3c38868e772ce9a2393591103c31d4a0a5693bb8
gerardbwww <44871943+gerardbwww@users.noreply.github.com>
2018-11-09 11:29:51 +0100

    Update Exercice1.java

--- src/pckg/Exercice1.java
+++ src/pckg/Exercice1.java
@@ -7,6 +7,7 @@ public class Exercice1 {
 
 		//Start of the program
 		System.out.print("Bonjour cher utilisateur, ceci est votre premier programme!");
+		System.out.print("Bonjour cher utilisateur, ceci est la seconde phrase!");
 	}
 
 }
